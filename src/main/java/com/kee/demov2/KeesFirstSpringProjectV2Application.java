d3d07d524e6d3836c2cdb3494563e27678fff25b
cajunkee <75953821+cajunkee@users.noreply.github.com>
2021-03-01 17:28:15 -0600

    inital commit

---
+++ src/main/java/com/kee/demov2/KeesFirstSpringProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.kee.demov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeesFirstSpringProjectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeesFirstSpringProjectV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kee/demov2/KeesFirstSpringProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kee.demov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeesFirstSpringProjectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
